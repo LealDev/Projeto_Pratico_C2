444bf44c2b79b7d7273345d34ce41c7fc52371df
LealDev <lealmaster0111@gmail.com>
2022-11-04 14:19:35 -0300

    Complete project

--- projeto.js
+++ projeto.js
@@ -1,4 +1,4 @@
-const colorsArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige",
+const coresArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige",
 "Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood",
 "CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk",
 "Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey",
@@ -7,7 +7,7 @@ const colorsArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Bei
 "DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet",
 "DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
 "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro",
-"GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow",
+"GhostWhite","Gold","GoldenRod","Gray","Green","GreenYellow",
 "HoneyDew","HotPink","IndianRed ","Indigo  ","Ivory","Khaki","Lavender",
 "LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral",
 "LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen",
@@ -25,46 +25,54 @@ const colorsArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Bei
 "Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow",
 "YellowGreen"]
 
+function randomColor() {
+    let cor = coresArray[Math.floor(Math.random() * coresArray.length)];
+    return cor;
+}
 let colorsInTheGame = []
 for (let i = 0; i < 10; i++) {
-  let colors = [colorsArray[Math.floor(Math.random() * colorsArray.length)]];
-  colorsInTheGame.push(colors);
+  let cor = randomColor();
+  colorsInTheGame.push(cor);
   for (let j = 0; j < colorsInTheGame.length; j++) {
-    if (colorsInTheGame[i]==colorsInTheGame[j]) {
-      colorsInTheGame[i].splice(colorsInTheGame[i],1, [colorsArray[Math.floor
-        (Math.random() * colorsArray.length)]])
+    if (colorsInTheGame[i]===colorsInTheGame[j]) {
+      colorsInTheGame.pop();
+      colorsInTheGame.push(randomColor());
     }
   }
 }
-function theGame(){
-  
-  correctChoose = [colorsArray[Math.floor(Math.random() * colorsArray.length)]]  
+colorsInTheGame.sort();
+console.log(colorsInTheGame)
+function test(winner) {
+  if (colorsInTheGame.includes(winner)){
+      return true;
+  }
+  return false;
+}
+
+function init(){
+  let correctColor = colorsInTheGame[Math.floor(Math.random() * colorsInTheGame.length)];
   while (true){
 
-      let choose = prompt("Choose and type the color \n" + colorsInTheGame[0] +
-      " "+ colorsInTheGame[1] +" "+ colorsInTheGame[2] + " " + 
-      colorsInTheGame[3] + " " + colorsInTheGame[4] + " " + 
-      colorsInTheGame[5] + " " + colorsInTheGame[6] + " " +
-      colorsInTheGame[7] + " " + colorsInTheGame[8] + " " +
-      colorsInTheGame[9]);
-
-      if (correctChoose == choose) {
-          document.body.style.backgroundColor = correctChoose;
-          alert("Congratulation!");
-          return true;
-      }
-      
-      if (!check(choose)){
-          alert("this is not a valid choice, please try again.");
-      } else if (choose > correctChoose) {
-          alert("Wrong! Color is alphabetically lower!");
-      } else if (choose < correctChoose){
-          alert("Wrong! Color is alphabetically lower!")
-      }
-      
+    let choose = prompt("Enter one of the colors that appears on the display: \n" +
+    colorsInTheGame[0]+" , "+colorsInTheGame[1]+" , "+
+    colorsInTheGame[2]+" , "+colorsInTheGame[3]+" , "+
+    colorsInTheGame[4]+" , "+colorsInTheGame[5]+" , "+
+    colorsInTheGame[6]+" , "+colorsInTheGame[7]+" , "+
+    colorsInTheGame[8]+" , "+colorsInTheGame[9]);
+    
+    if (correctColor === choose) {
+      document.body.style.backgroundColor = correctColor;
+      alert("congratulation");
+      return true;
   }
-  
-
+    if (!test(choose)){
+      alert( "Please, type a valid color");
+  } else if (choose > correctColor) {
+    alert("Wrong. the color i chose is alphabetically lower");
+  } else if (choose < correctColor){
+    alert("Wrong. the color i chose is alphabetically higher");
+  }  
+  }
 }
 
 
