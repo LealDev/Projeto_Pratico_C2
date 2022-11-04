d8dd4e7a504d17d32cf7d4ee6f651253bb38d4da
LealDev <lealmaster0111@gmail.com>
2022-11-04 14:18:53 -0300

    completeProject

--- testeRandom.js
+++ testeRandom.js
@@ -1,58 +1,32 @@
-var cores = [ "AliceBlue",
-"AntiqueWhite",
-"Aqua",
-"Aquamarine",
-"Azure",
-"Beige",
-"Bisque",
-"Black",
-"BlanchedAlmond",
-"Blue",
-"BlueViolet",
-"Brown",
-"BurlyWood",
-"CadetBlue",
-"Chartreuse",
-"AliceBlue",
-"AntiqueWhite",
-"Aqua",
-"Aquamarine",
-"Azure",
-"Beige",
-"Bisque",
-"Black",
-"BlanchedAlmond",
-"Blue",
-"BlueViolet",
-"Brown",
-"BurlyWood",
-"CadetBlue",
-"Chartreuse","AliceBlue",
-"AntiqueWhite",
-"Aqua",
-"Aquamarine",
-"Azure",
-"Beige",
-"Bisque",
-"Black",
-"BlanchedAlmond",
-"Blue",
-"BlueViolet",
-"Brown",
-"BurlyWood",
-"CadetBlue",
-"Chartreuse",
-];
-let colorsInTheGame = []
-for (let i = 0; i < 10; i++) {
-  let colors = [cores[Math.floor(Math.random() * cores.length)]];
-  colorsInTheGame.push(colors);
-  for (let j = 0; j < colorsInTheGame.length; j++) {
-    if (colorsInTheGame[i]===colorsInTheGame[j]) {
-      colorsInTheGame[i].splice(colorsInTheGame[i],1, [cores[Math.floor(Math.random() * cores.length)]])
-    }
-  }
-}
+const cores = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige",
+"Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood",
+"CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk",
+"Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey",
+"DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange",
+"DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue",
+"DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet",
+"DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+"FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro",
+"GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow",
+"HoneyDew","HotPink","IndianRed ","Indigo  ","Ivory","Khaki","Lavender",
+"LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral",
+"LightCyan"]
+
+const cores1 =["LightGoldenRodYellow","LightGray","LightGrey","LightGreen",
+"LightPink","LightSalmon" ,"LightSeaGreen","LightSkyBlue","LightSlateGray",
+"LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen",
+"Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+"MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen",
+"MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose",
+"Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange",
+"OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise",
+"PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+"Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon",
+"SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue",
+"SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal",
+"Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow",
+"YellowGreen"]
 
 
-console.log(colorsInTheGame);
+
+console.log(colorsInTheGame)
